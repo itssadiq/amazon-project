54f8bdadf6e1a3616e51d18a6452e24bd6470f7d
itssadiq <sadiqalishaikh009@gmail.com>
2025-04-09 17:53:03 +0500

    Made selector element interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -29,7 +29,7 @@ products.forEach((product) => {
           )}</div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -63,7 +63,14 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
+    const selectorElement = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+
+    const selectorQuantity = Number(selectorElement.value);
+
     let matchingItem;
+
     cart.forEach((item) => {
       if (productId === item.productId) {
         matchingItem = item;
@@ -71,11 +78,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += selectorQuantity;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: selectorQuantity,
       });
     }
 
